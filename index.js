2e61270af8aff1dcef779fed91f89454f6520537
unknown <tiavinartjjonathan@gmail.com>
2024-03-21 10:06:32 +0300

    add some route

--- index.js
+++ index.js
@@ -7,21 +7,38 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-  server.use(cors());
+server.use(cors());
 
-  const io = socketIO(server);
-
-  io.origins('*:*');
-
-
-  io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
+app.get('/', (req, res) => {
+  res.json({
+    data: 'Hello World'
   });
+});
 
-  setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+app.get('/:name', (req, res) => {
+  let name = req.params.name;
 
-  io.socket.on("message", function(data) {
-    // When a "message" event is received, emit another "message" event with the same data
-    io.emit("message", data);
+  res.json({
+    data: `Hello ${name}`
+  });
+});
+
+
+
+// socket handler
+const io = socketIO(server);
+
+io.origins('*:*');
+
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+io.socket.on("message", function (data) {
+  // When a "message" event is received, emit another "message" event with the same data
+  io.emit("message", data);
 });
